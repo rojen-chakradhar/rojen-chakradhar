a55b1530f6d613a2770cd3abab17fc128ad6ed84
Rojen Chakradhar <chakradhar@rojen.me>
2024-04-30 20:14:24 +0545

    locomotive js removed

--- scripts/gsap.js
+++ scripts/gsap.js
@@ -1,32 +1,3 @@
-gsap.registerPlugin(ScrollTrigger);
-const locoScroll = new LocomotiveScroll({
-	el: document.querySelector("#main"),
-	smooth: true,
-});
-locoScroll.on("scroll", ScrollTrigger.update);
-ScrollTrigger.scrollerProxy("#main", {
-	scrollTop(value) {
-		return arguments.length
-			? locoScroll.scrollTo(value, 0, 0)
-			: locoScroll.scroll.instance.scroll.y;
-	},
-	getBoundingClientRect() {
-		return {
-			top: 0,
-			left: 0,
-			width: window.innerWidth,
-			height: window.innerHeight,
-		};
-	},
-	pinType: document.querySelector("#main").style.transform
-		? "transform"
-		: "fixed",
-});
-ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-ScrollTrigger.refresh();
-
-//--------------------------------------------------------------------------------------------------
-
 let nav = gsap.timeline();
 
 nav.from("nav a h1", {
@@ -62,7 +33,7 @@ gsap.to("#page2 h1", {
 	scrollTrigger: {
 		// markers: true,
 		trigger: "#page2",
-		scroller: "#main",
+		scroller: "body",
 		start: "top 0%",
 		end: "bottom 0",
 		scrub: 3,
--- scripts/locomotive.js
+++
@@ -1,5 +0,0 @@
-const scroll = new LocomotiveScroll({
-	el: document.querySelector("#main"),
-	smooth: true,
-	lerp: 0.05,
-});
